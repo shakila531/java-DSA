bec5a6b5f18913df92cce82e19c301d7224cb6f5
shakila531 <balkisbegum415@gmail.com>
2026-07-07 16:12:39 +0530

    add new filr

---
+++ Perfectnum.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Perfectnum{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int sum = 0;
+        for(int i = 1; i < n; i++){
+            if(n % i == 0){
+                sum = sum + i;
+            }
+        }
+        if(sum == n){
+            System.out.println("Perfect number");
+        }
+        else{
+            System.out.println("Not a perfect number");
+        }
+    }
+}
